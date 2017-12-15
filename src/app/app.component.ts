8d645f56458fdd04348064c4ec30b1aa0229d265
hussain <mukadam59@gmail.com>
2017-12-15 21:24:01 +0530

    fix: links, images and font color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,20 @@ export class AppComponent {
 
   openLink() {
     window.open('https://speakerdeck.com/hsm59/kotlin-101', '_blank');
+  }
+  openGithubProfile() {
+    window.open('https://github.com/hsm59', '_blank');
+  }
+  openSOProfile() {
+    window.open('https://stackoverflow.com/users/1992823/hsm59', '_blank');
+  }
+  openLinkedInProfile() {
+    window.open('https://www.linkedin.com/in/mukadam59/', '_blank');
+  }
+  openMediumBlog() {
+    window.open('https://medium.com/@mukadam59', '_blank');
+  }
+  openTwitterProfile() {
+    window.open('https://twitter.com/HSM59', '_blank');
   }
 }
